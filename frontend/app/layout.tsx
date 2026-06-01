f874aa9969dedd67310bfa4968c1125f23cd2408
mrithul <Mridhul@radiantwtech.in>
2026-06-01 16:56:47 +0530

    update warnings

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col bg-white text-gray-900">
+      <body className="min-h-full flex flex-col bg-white text-gray-900" suppressHydrationWarning>
         <Providers>
           <Navbar />
           <main className="flex-grow">
